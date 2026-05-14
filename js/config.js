15d57489f2e35347c618043f824457dc82071f9d
Mosechu047 <mosesagoya0@gmail.com>
2026-05-14 09:45:09 +0300

    updated

---
+++ js/config.js
@@ -0,0 +1,31 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAhBHuXs21MbzOaCjl2pIdj16HS4wKoYTY",
+  authDomain: "attendance-5dc0f.firebaseapp.com",
+  databaseURL: "https://attendance-5dc0f-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "attendance-5dc0f",
+  storageBucket: "attendance-5dc0f.firebasestorage.app",
+  messagingSenderId: "545210256586",
+  appId: "1:545210256586:web:ec7ab483a88ca1af0b3833",
+  measurementId: "G-01WBHVCKZ3"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+
+<<<<<<< HEAD:attendance/js/config.js
+console.log('connected to firebase');
+
+function logout() {
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    window.location.href = "index.html";
+  }).catch((error) => {
+    // An error happened.
+    alert("Error signing out: " + error.message)
+    console.log(error);
+  })
+}
+=======
+console.log('connected to firebase');
+>>>>>>> 02d193ff393d83480d1afca735f96fd38c0047f9:js/config.js
---
+++ js/reset.js
@@ -0,0 +1,17 @@
+let resetBtn = document.getElementById('resetBtn');
+
+resetBtn.addEventListener('click', (event) => {
+    
+    let txtemail = document.getElementById("txtemail").value;
+    resetBtn.innerHTML = "Sending Reset Link...";
+    auth.sendPasswordResetEmail(txtemail)
+        .then(() => {
+            alert("Password reset email sent. Please check your inbox(if email exists in the system).");
+            txtemail.value = "";
+        
+        })
+        .catch((error) => {
+            alert("Error: " + error.message);
+            console.log(error);
+        });
+});
